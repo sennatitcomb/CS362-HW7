f0a9ee68061f85e914c1320c4381f3d44d499462
Senna <sennatitcomb@gmail.com>
2021-05-27 14:12:00 -0700

    Misunderstood assignment a bit and talked to TA, Wrote first test properly, (tests if fizzbuzz is able to print out number 1 to 100)

--- fizzbuzz.py
+++ fizzbuzz.py
@@ -1,11 +1,7 @@
 def fizzbuzz():
-    for num in range (1, 100):
-        if num %3  == 0 and num % 5 != 0:
-            return "Fizz"  
-        elif num % 5 == 0 and num % 3 != 0:
-            return "Buzz"
-        elif num % 3 == 0 and num % 5 == 0:
-            return "FizzBuzz"
-        else: 
-            return num
+    num = 1
+    while num < 100:
+            print(num)
+            num+=1
+   # return count
 
