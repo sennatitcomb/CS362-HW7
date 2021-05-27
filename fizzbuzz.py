d4c34a0747b6a9864b802476e4f603060b63aa17
Senna <sennatitcomb@gmail.com>
2021-05-27 14:19:03 -0700

    Implemented changes: num starts at 2 and increases until equal to 100. String was created and is used to print out numbers 1 to 100 correctly to fulfill first test

--- fizzbuzz.py
+++ fizzbuzz.py
@@ -1,7 +1,9 @@
 def fizzbuzz():
-    num = 1
-    while num < 100:
-            print(num)
+    num = 2
+    string = "1"
+    while num <= 100:
+            #print(num)
+            string+=", "+str(num)
             num+=1
-   # return count
+    return string
 
