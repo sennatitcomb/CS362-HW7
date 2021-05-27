bf850d69b65afac117a04096944b3ffd5e4af9fa
Senna <sennatitcomb@gmail.com>
2021-05-27 14:28:52 -0700

    Implemented and statement in if statement to check if number is mulitple of 3 and not a multiple of 5

--- fizzbuzz.py
+++ fizzbuzz.py
@@ -2,7 +2,7 @@ def fizzbuzz():
     num = 2
     string = "1"
     while num <= 100:
-        if num %3  == 0:
+        if num %3  == 0 and num % 5 != 0:
             string+=", Fizz"
         else:
             #print(num)
